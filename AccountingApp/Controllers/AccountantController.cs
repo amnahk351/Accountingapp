f1c7d373f726391238c452bd1d126e0fdcddef0f
amnahk351 <46543619+amnahk351@users.noreply.github.com>
2019-02-19 17:17:51 -0500

    Committing changes to branch

---
+++ AccountingApp/Controllers/AccountantController.cs
@@ -0,0 +1,26 @@
+﻿using AccountingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountingApp.Controllers
+{
+    public class AccountantController : Controller
+    {
+        // GET: Accountant
+        //public ActionResult AccountantIndex()
+        //{
+        //    return View();
+        //}
+        public ActionResult AccountantIndex()
+        {
+            Database1Entities3 db = new Database1Entities3();
+            var getaccountslist = db.ChartOfAccs.ToList();
+            SelectList list = new SelectList(getaccountslist,"AccountNumber", "AccountName");
+            ViewBag.accountlist = list;
+            return View();
+        }
+    }
+}
