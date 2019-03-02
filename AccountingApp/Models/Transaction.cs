6dba0dfd1b6bcfa0644b861e955062ea8a2a9ec2
coltMcKissick <colt.mckissick@gmail.com>
2019-03-02 15:58:47 -0500

    adding transaction model

---
+++ AccountingApp/Models/Transaction.cs
@@ -0,0 +1,27 @@
+namespace AccountingApp.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Transaction")]
+    public partial class Transaction
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public double AccountNumber { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Debit { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Credit { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Status { get; set; }
+    }
+}
