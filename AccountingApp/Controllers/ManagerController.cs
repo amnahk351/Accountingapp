59810812ff95358968fdbe3a1d1e2c1702553e92
coltMcKissick <colt.mckissick@gmail.com>
2019-03-16 10:11:40 -0400

    formatted table so that each entry has own row

--- AccountingApp/Controllers/ManagerController.cs
+++ AccountingApp/Controllers/ManagerController.cs
@@ -18,8 +18,34 @@ namespace AccountingApp.Controllers
 
         public ActionResult ManagerApproval()
         {
-            var allTransactions = db.Transactions.ToList();
-            return View(allTransactions);
+            var allPendingTransactions = db.Transactions.Where(t => t.Status == "pending").ToList();
+
+            Entries entries = new Entries();
+            List<int> ids = new List<int>();
+            foreach (Transaction t in allPendingTransactions)
+            {
+                int id = t.EntryId.Value;
+                string status = t.Status;
+
+                if (ids.Contains(id))
+                    continue;
+                else
+                    ids.Add(id);
+
+                Entry e = new Entry(id, status);
+                foreach (Transaction t2 in allPendingTransactions)
+                {
+                    if (t2.EntryId == id)
+                    {
+                        e.accountNames.Add(t2.AccountName);
+                        e.debits.Add(t2.Debit.Value);
+                        e.credits.Add(t2.Credit.Value);
+                    }
+                }
+                entries.entries.Add(e);
+            }
+
+            return View(entries);
         }
 
         public ActionResult GeneralJournal()
---
+++ AccountingApp/Models/Entry.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountingApp.Models
+{
+    public class Entry
+    {
+        public List<Transaction> transactions { get; set; }
+        public int entryID { get; set; }
+        public string status { get; set; }
+        public List<String> accountNames { get; set; }
+        public List<Decimal> debits { get; set; }
+        public List<Decimal> credits { get; set; }
+
+        public Entry(int entryID, string status)
+        {
+            this.entryID = entryID;
+            this.status = status;
+            transactions = new List<Transaction>();
+            accountNames = new List<String>();
+            debits = new List<Decimal>();
+            credits = new List<Decimal>();
+        }
+
+        public string FormattedAccountNames() {
+
+            string formatted = "";
+
+            foreach (string account in accountNames)
+            {
+                if (formatted != "")
+                {
+                    formatted += "\r\n";
+                }
+                formatted += account;
+            }
+
+            return formatted;
+        }
+
+        public string FormattedDebits()
+        {
+
+            string formatted = "";
+
+            foreach (Decimal debit in debits)
+            {
+                if (formatted != "")
+                {
+                    formatted += "\r\n";
+                }
+                formatted += debit.ToString();
+            }
+
+            return formatted;
+        }
+
+        public string FormattedCredits()
+        {
+
+            string formatted = "";
+
+            foreach (Decimal credit in credits)
+            {
+                if (formatted != "")
+                {
+                    formatted += "\r\n";
+                }
+                formatted += credit.ToString();
+            }
+
+            return formatted;
+        }
+    }
+}
