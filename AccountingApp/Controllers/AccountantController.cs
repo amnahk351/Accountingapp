ec3b601d6fcee1bf7cb4555ba2f0c3d6f5b09d71
bthayer3 <btwildcat13@gmail.com>
2019-04-09 22:23:13 -0400

    Journalizing fix

--- AccountingApp/Controllers/AccountantController.cs
+++ AccountingApp/Controllers/AccountantController.cs
@@ -105,14 +105,13 @@ namespace AccountingApp.Controllers
             int insertedRecords = 0;
             int NewEntryId = GetLatestEntryId();
             var sessionUser = Session["Username"] as string;
+            EventLogHandler Logger = new EventLogHandler();
 
             using (IDbConnection db = new SqlConnection(SqlAccess.GetConnectionString()))
             {
 
-
                 for (int i = 1; i < transactions.Length; i++)
                 {
-
                     //var x = DateTime.Now;
 
                     //if was submitted today added the current time to the database
@@ -123,7 +122,7 @@ namespace AccountingApp.Controllers
                     string sql = $"Insert into dbo.TransactionTable (AccountantUsername, AccountantComment, " +
                     "DateSubmitted, Status, AccountName, Debit, Credit, EntryId, Entry_Type)" +
                     "values(@AccountantUsername,@AccountantComment,@DateSubmitted,@Status,@AccountName," +
-                    "@Debit,@Credit,@EntryId,EntryType)";
+                    "@Debit,@Credit,@EntryId,@Entry_Type)";
 
                     db.Execute(sql, new
                     {
@@ -135,11 +134,11 @@ namespace AccountingApp.Controllers
                         Debit = transactions[i].Debit,
                         Credit = transactions[i].Credit,
                         EntryId = NewEntryId + 1,
-                        EntryType = transactions[i].Entry_Type
+                        Entry_Type = transactions[i].Entry_Type
                     });
 
                     insertedRecords++;
-
+                    //Logger.LogJournalEntrySubmitted(sessionUser, NewEntryId.ToString());
                 }
             }
 
